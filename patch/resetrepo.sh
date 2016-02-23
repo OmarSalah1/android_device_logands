604ded1829dc1630f23585036c3e79d8458beb52
ZIM <zim555@mail.ru>
2016-02-23 09:36:41 +0200

    Add diff patch and reset repo script

---
+++ patch/resetrepo.sh
@@ -0,0 +1,24 @@
+cd frameworks/av
+git reset --hard
+cd ../..
+cd frameworks/base
+git reset --hard
+cd ../..
+cd frameworks/native
+git reset --hard
+cd ../..
+cd frameworks/opt/telephony
+git reset --hard
+cd ../../..
+cd frameworks/opt/telephony-msim
+git reset --hard
+cd ../../..
+cd hardware/broadcom/libbt
+git reset --hard
+cd ../../..
+cd packages/apps/Settings
+git reset --hard
+cd ../../..
+cd packages/services/Telephony
+git reset --hard
+cd ../../..
