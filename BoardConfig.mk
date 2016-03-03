2e79a5ffdb3f6c46118f3f208da94eab99287f16
ZIM <zim555@mail.ru>
2016-03-03 19:52:30 +0200

    New vendor lib folder

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/samsung/logands/BoardConfigVendor.mk
+-include vendor/samsung/loganxx/BoardConfigVendor.mk
 
 # Platform
 TARGET_ARCH := arm
